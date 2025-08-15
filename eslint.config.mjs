3aaf99e85adca4abcab0f1f868dffddd16a14d42
Nuroldin U. Pimping <nuroldin.umpara.pimping08@gmail.com>
2025-08-15 16:35:41 +0800

    feat: add initial layout and components for dashboard, including Menu and Navbar

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,32 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
+	recommendedConfig: true,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.config({
+		extends: [
+			"next/core-web-vitals",
+			"next/typescript",
+			"standard",
+			"plugin:tailwindcss/recommended",
+			"prettier",
+		],
+		rules: {
+			semi: ["error", "always"],
+			"prefer-arrow-callback": ["error"],
+			"prefer-template": ["error"],
+			"no-undef": "off",
+		},
+		settings: {
+			react: {
+				version: "detect",
+			},
+		},
+		ignorePatterns: [".next/", "node_modules/"],
+	}),
 ];
 
 export default eslintConfig;
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,8 +2,14 @@ export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
+  
 }>) {
   return (
-    <div>{children}</div>
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-]14%] bg-red-200">l</div>
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-green-200">r</div>
+    </div>
   );
 }

50cc0929b767852514ef17759ad3be2e20569799
Nuroldin U. Pimping <nuroldin.umpara.pimping08@gmail.com>
2025-09-19 16:45:19 +0800

    feat: Implement student dashboard with calendar and announcements
    - Added BigCalendar component to display the schedule for students.
    - Integrated EventCalendar and Announcements components for additional information.
    - Updated Menu component to conditionally render items based on user role.
    - Created data structure for roles and various entities (teachers, students, parents, etc.) in lib/data.ts.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { role } from "@/lib/data";
 
 const menuItems = [
 	{
@@ -124,16 +125,20 @@ const Menu = () => {
 					<span className="hidden lg:block text-gray-400 font-light my-4">
 						{section.title}
 					</span>
-					{section.items.map((item) => (
-						<Link
-							href={item.href}
-							key={item.label}
-							className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
-						>
-							<Image src={item.icon} alt={item.label} width={24} height={24} />
-							<span className="hidden lg:block">{item.label}</span>
-						</Link>
-					))}
+					{section.items.map((item) => {
+						if (item.visible.includes(role)) {
+							return (
+								<Link
+									href={item.href}
+									key={item.label}
+									className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+								>
+									<Image src={item.icon} alt="" width={20} height={20} />
+									<span className="hidden lg:block">{item.label}</span>
+								</Link>
+							);
+						}
+					})}
 				</div>
 			))}
 		</div>
